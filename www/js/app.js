7384cb765b802cd6901476208b23eb429aff9bb4
Jain, Mayur Mayur.Jain@parc.com <Mayur.Jain@parc.com>
2015-10-25 02:18:04 -0700

    iframe for plugin download

--- www/js/app.js
+++ www/js/app.js
@@ -32,20 +32,11 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
+  .state('app.plugin', {
+      url: '/plugin',
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
+          templateUrl: 'templates/plugin.html'
         }
       }
     })
@@ -59,15 +50,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     });
 
-  /*.state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });*/
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/cards');
 });
